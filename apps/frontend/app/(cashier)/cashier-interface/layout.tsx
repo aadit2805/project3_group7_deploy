0aaacad75e6a52c01296a02e21b40c83a696adc3
Yajat Melwani <yajatmelwani@tamu.edu>
2025-11-07 11:09:01 -0600

    Added cashier interface

---
+++ apps/frontend/app/(cashier)/cashier-interface/layout.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import OrderPane from '@/app/components/OrderPane';
+
+export default function CashierLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex">
+      <div className="w-2/3">{children}</div>
+      <OrderPane />
+    </div>
+  );
+}
+
